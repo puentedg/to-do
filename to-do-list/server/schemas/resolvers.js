c5ab074143b060117674027f0ad77350d8e4c296
puentedg <puentedg@gmail.com>
2022-11-27 07:20:00 -0500

    profile model

--- to-do-list/server/schemas/resolvers.js
+++ to-do-list/server/schemas/resolvers.js
@@ -1,65 +1,29 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { Profile } = require('../models');
-const { signToken } = require('../utils/auth');
+const { Todo } = require('../models');
 
 const resolvers = {
+
   Query: {
-    profiles: async () => {
-      return Profile.find();
-    },
-
-    profile: async (parent, { profileId }) => {
-      return Profile.findOne({ _id: profileId });
-    },
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+    todos: () => Todo.find()
   },
-
   Mutation: {
-    addProfile: async (parent, { name, email, password }) => {
-      const profile = await Profile.create({ name, email, password });
-      const token = signToken(profile);
-
-      return { token, profile };
+    createTodo: async (_, { text }) => {
+      const todo = new Todo({ text, complete: false });
+      
+      await todo.save();
+      return todo;
     },
-    login: async (parent, { email, password }) => {
-      const profile = await Profile.findOne({ email });
-
-      if (!profile) {
-        throw new AuthenticationError('No profile with this email found!');
-      }
-
-      const correctPw = await profile.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect password!');
-      }
-
-      const token = signToken(profile);
-      return { token, profile };
+    updateTodo: async (_, { id, complete }) => {
+   
+      await Todo.findByIdAndUpdate(id, {complete});
+      return true;
     },
+    removeTodo: async (_, { id }) => {
 
-    addSkill: async (parent, { profileId, skill }) => {
-      return Profile.findOneAndUpdate(
-        { _id: profileId },
-        {
-          $addToSet: { skills: skill },
-        },
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
-    },
-    removeProfile: async (parent, { profileId }) => {
-      return Profile.findOneAndDelete({ _id: profileId });
-    },
-    removeSkill: async (parent, { profileId, skill }) => {
-      return Profile.findOneAndUpdate(
-        { _id: profileId },
-        { $pull: { skills: skill } },
-        { new: true }
-      );
-    },
-  },
-};
+      await Todo.findByIdAndRemove(id);
+      return true;
+    }
+  }
+}
 
 module.exports = resolvers;
